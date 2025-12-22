3eaa082fb3ee7bfcdc44f60dcedf54a5989fb509
Ahmad Subhan <asdcode123@gmail.com>
2025-12-23 05:09:42 +0700

    fix(backend): worker stop working

--- railway/run-worker.sh
+++ railway/run-worker.sh
@@ -3,4 +3,4 @@
 
 # This command runs the queue worker.
 # An alternative is to use the php artisan queue:listen command
-php artisan queue:work --sleep=3 --tries=1 --memory=1024
+php artisan queue:work --sleep=3 --tries=3 --memory=1024 --timeout=0
